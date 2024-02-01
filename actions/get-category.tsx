b89e5635ddb97f099bab1a6a2c7254699c5f0f09
chandiimanpreet <manpreetchandi555@gmail.com>
2024-02-02 02:04:37 +0530

    some changes

--- actions/get-category.tsx
+++ actions/get-category.tsx
@@ -2,7 +2,7 @@ import { Category } from "@/types";
 
 
 const getCategory = async (id: string): Promise<Category> => {
-  const URL = `https://e-commerce--admin.vercel.app//api/021c64e9-eba0-47f1-b365-aa1588550d1d/categories`;
+  const URL = `https://e-commerce--admin.vercel.app/api/021c64e9-eba0-47f1-b365-aa1588550d1d/categories`;
   const res = await fetch(`${URL}/${id}`);
 
   return res.json();
