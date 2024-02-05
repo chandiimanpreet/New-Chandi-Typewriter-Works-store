b769bcee4a3804d6814579bf35baa8f5516924c6
chandiimanpreet <manpreetchandi555@gmail.com>
2024-02-05 22:40:14 +0530

    small bug of api

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,7 +3,7 @@ import { Category } from "@/types";
 
 const getCategories = async (): Promise<Category[]> => {
 
-  const URL = `https://e-commerce--admin.vercel.app/021c64e9-eba0-47f1-b365-aa1588550d1d/categories`;
+  const URL = `https://e-commerce--admin.vercel.app/api/021c64e9-eba0-47f1-b365-aa1588550d1d/categories`;
   const res = await fetch(URL);
 
   return res.json();
