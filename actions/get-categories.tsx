23e1205dce93d3329ba08064e04415f4e3a516f1
manpreet <manpreetchandi555@gmail.com>
2024-11-24 22:48:58 +0530

    small changes

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -1,11 +1,13 @@
 import { Category } from "@/types";
 
-
 const getCategories = async (): Promise<Category[]> => {
-
-  const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories` || "https://new-chandi-admin-vercel.vercel.app/api/4452bdaf-6c8d-4c79-b1dd-ccc0d9adf9b7/categories";
+  const URL =
+    `${process.env.NEXT_PUBLIC_API_URL}/categories` ||
+    "https://new-chandi-admin-vercel.vercel.app/api/4452bdaf-6c8d-4c79-b1dd-ccc0d9adf9b7/categories";
   const res = await fetch(URL);
 
+
+  
   return res.json();
 };
 
--- actions/get-category.tsx
+++ actions/get-category.tsx
@@ -1,8 +1,9 @@
 import { Category } from "@/types";
 
-
 const getCategory = async (id: string): Promise<Category> => {
-  const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories` || "https://new-chandi-admin-vercel.vercel.app/api/4452bdaf-6c8d-4c79-b1dd-ccc0d9adf9b7/categories";
+  const URL =
+    `${process.env.NEXT_PUBLIC_API_URL}/categories` ||
+    "https://new-chandi-admin-vercel.vercel.app/api/4452bdaf-6c8d-4c79-b1dd-ccc0d9adf9b7/categories";
   const res = await fetch(`${URL}/${id}`);
 
   return res.json();
